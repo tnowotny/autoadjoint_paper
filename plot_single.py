cfb2cddac0123b1c3c3fac4232c427a0270dd2c0
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2026-03-03 10:26:50 +0000

    Added another plotting script for not crossvalidation runs.

---
+++ plot_single.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import matplotlib
+
+cmap = matplotlib.colormaps["jet"]
+
+d = []
+labels = []
+epochs = []
+for k in range(len(sys.argv)-1):
+    d.append(np.loadtxt(sys.argv[k+1]))
+    epochs.append(int(np.max(d[k][:,1]))+1)
+    
+    with open(sys.argv[k+1]) as f:
+        labels.append(f.readline().strip("\n").split(" "))
+        labels[k].pop(0)
+colN = [ d[i].shape[1] for i in range(len(d)) ]
+
+plotN = min(colN)
+#figure out what epochs were calculated
+max_epoch = max(epochs)
+x_axis = []
+for k in range(len(d)):
+    base = np.ones(epochs[k])
+    steps = np.arange(0,max_epoch*10,max_epoch)
+    x_axis.append( np.outer(base,steps).T.flatten())
+
+wd = int(np.sqrt(plotN))+1
+ht = plotN // wd +1
+
+fig, ax = plt.subplots(ht, wd, sharex=True)
+
+plotx = []
+splits = []
+for k in range (len(d)):
+    split = np.where(np.diff(d[k][:,0]) < 0)
+    x = [0]+list(split[0])+[len(d[k][:,1])-1]
+    the_x = []
+    for i in range(len(x)-1):
+        the_x.append(np.arange(x[i+1]-x[i])+i*max_epoch)
+    plotx.append(the_x)
+    splits.append(x)
+
+cstep = 255 // len(d)
+avg = np.zeros((len(d),plotN))
+sig = np.zeros((len(d),plotN))
+print(labels)
+for y in range(ht):
+    for x in range(wd):
+        i = y*wd+x
+        if i < plotN:
+            for k in range (len(d)):
+                for j in range(len(splits[k])-1):
+                    #print(f"d[{k}][{splits[k][j]+1}:{splits[k][j+1]+1}]")
+                    ax[y,x].plot(plotx[k][j],d[k][splits[k][j]+1:splits[k][j+1]+1,i],color=cmap(k*cstep),lw=1)
+                avg[k,i] = np.mean(d[k][splits[k][1:],i])
+                sig[k,i] = np.std(d[k][splits[k][1:],i])
+                
+            ax[y,x].set_title(labels[0][i])
+        if i == plotN - 1:
+            ax[y,x].set_ylim([0 ,1])
+
+print(avg)
+print(sig)
+
+np.save("fold_avg",avg)
+np.save("fold_std",sig)
+
+plt.show()
+                        
